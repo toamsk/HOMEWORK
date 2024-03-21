e056f7a3bdf6ebc0bb7b6dd3298c9d6d01613f77
Olga <otimoshenko820@gmail.com>
2024-03-21 20:39:51 +0300

    Задания четвертого урока с исправлениями

--- lesson4/test_string_utils.py
+++ lesson4/test_string_utils.py
@@ -1,132 +1,107 @@
 import pytest
 from string_utils import StringUtils
 
-stringUtils = StringUtils()
+string_Utils = StringUtils()
 
 # Принимает на вход текст, делает первую букву заглавной и возвращает этот же текст
-# Позитивные проверки
-def test_positive_string():
-    stringUtils = StringUtils()
-    res = stringUtils.capitilize("лондон")
-    assert res == "Лондон"
 
-# Негативные проверки
-@pytest.mark.negativ_test
-def test_negatitive_string():
-    stringUtils = StringUtils()
-    res = stringUtils.capitilize("лондон")
-    assert res == "лондон"
-
-@pytest.mark.negativ_test
-def test_negatitive_string2():
-    stringUtils = StringUtils()
-    res = stringUtils.capitilize("лондон")
-    assert res == "7ондон"
-
-@pytest.mark.negativ_test    
-def test_negatitive_string3():
-    stringUtils = StringUtils()
-    res = stringUtils.capitilize("лондон")
-    assert res == "$ондон"
-
-@pytest.mark.negativ_test
-def test_negatitive_string4():
-    stringUtils = StringUtils()
-    res = stringUtils.capitilize("лондон")
-    assert res == ":ондон"
+@pytest.mark.parametrize( 'text1, result', [("лондон", "Лондон"), ("лондон", "лондон"), ("лондон", "7ондон"), ("лондон", "$ондон"), ("лондон", ":ондон")] )
+def test_positive_string(text1, result):
+    string_Utils = StringUtils()
+    res = string_Utils.capitilize(text1)
+    assert res == result
 
 # Принимает на вход текст и удаляет пробелы в начале, если они есть
-def test_positive_trim():
-    stringUtils = StringUtils()
-    res = stringUtils.trim(" Париж")
-    assert res == "Париж"
 
-def test_positive_trim2():
-    stringUtils = StringUtils()
-    res = stringUtils.trim("   Париж")
-    assert res == "Париж"
+@pytest.mark.parametrize( 'text2, result2', [(" Париж", "Париж"), ("  Париж", "Париж"), ("Париж", "Париж"), (" ", "")] )
+def test_positive_trim2(text2, result2):
+    string_Utils = StringUtils()
+    res = string_Utils.trim(text2)
+    assert res == result2
 
 # Принимает на вход текст с разделителем и возвращает список строк. \n
-@pytest.mark.skip(reason="Тест падает")
+#@pytest.mark.skip(reason="Тест падает, найден баг")
+@pytest.mark.xfail
 def test_positive_to_list():
-    stringUtils = StringUtils()
+    string_Utils = StringUtils()
     to_list = []
-    res = stringUtils.to_list("яблоко, мандарин, апельсин, киви")
+    res = string_Utils.to_list("яблоко, мандарин, апельсин, киви")
     assert res == ["яблоко", "мандарин", "апельсин", "киви"]
 
 def test_positivee_to_list():
-    stringUtils = StringUtils()
+    string_Utils = StringUtils()
     to_list = []
-    res = stringUtils.to_list("черника:виноград:ежевика:клубника", ":")
+    res = string_Utils.to_list("черника:виноград:ежевика:клубника", ":")
     assert res == ["черника", "виноград", "ежевика", "клубника"]
 
 # Возвращает `True`, если строка содержит искомый символ и `False` - если нет \n 
 def test_positive_contains():
-    stringUtils = StringUtils()
-    res = stringUtils.contains("Mississippi", "s")
-    assert res == True
+    string_Utils = StringUtils()
+    res = string_Utils.contains("Mississippi", "s")
+    assert res is True
 
 def test_positive_contains2():
-    stringUtils = StringUtils()
-    res = stringUtils.contains("Mississippi", "A")
-    assert res == False
+    string_Utils = StringUtils()
+    res = string_Utils.contains("Mississippi", "A")
+    assert res is False
 
+def test_positive_contains2():
+    string_Utils = StringUtils()
+    res = string_Utils.contains("Mississippi", " ")
+    assert res is False
+  
 # Удаляет все подстроки из переданной строки \n 
-def test_positive_del_sym():
-    stringUtils = StringUtils()
-    res = stringUtils.delete_symbol("Mississippi", "s")
-    assert res == "Miiippi"
-
-def test_positive_del_sym2():
-    stringUtils = StringUtils()
-    res = stringUtils.delete_symbol("Mississippi", "sip")
-    assert res == "Missispi"
+@pytest.mark.parametrize( 'text3, text4, result3', [("Mississippi", "s", "Miiippi"), ("Mississippi", "sip", "Missispi") ] )
+def test_positive_delete_symbol(text3, text4, result3):
+    string_Utils = StringUtils()
+    res = string_Utils.delete_symbol(text3, text4)
+    assert res == result3
 
 # Возвращает `True`, если строка начинается с заданного символа и `False` - если нет \n 
 def test_positive_start_wi():
-    stringUtils = StringUtils()
-    res = stringUtils.starts_with("Stepnogorsk", "S")
-    assert res == True
+    string_Utils = StringUtils()
+    res = string_Utils.starts_with("Stepnogorsk", "S")
+    assert res is True
 
 def test_positive_start_wi2():
-    stringUtils = StringUtils()
-    res = stringUtils.starts_with("Stepnogorsk", "G")
-    assert res == False
+    string_Utils = StringUtils()
+    res = string_Utils.starts_with("Stepnogorsk", "G")
+    assert res is False
         
 # Возвращает `True`, если строка заканчивается заданным символом и `False` - если нет \n 
 def test_positive_end_wi():
-    stringUtils = StringUtils()
-    res = stringUtils.end_with("Moscow", "w")
-    assert res == True
+    string_Utils = StringUtils()
+    res = string_Utils.end_with("Moscow", "w")
+    assert res is True
 
 def test_positive_end_wi2():
-    stringUtils = StringUtils()
-    res = stringUtils.end_with("Moscow", "o")
-    assert res == False
+    string_Utils = StringUtils()
+    res = string_Utils.end_with("Moscow", "o")
+    assert res is False
 
 # Возвращает `True`, если строка пустая и `False` - если нет \n 
 def test_positive_is_emply():
-    stringUtils = StringUtils()
-    res = stringUtils.is_empty("")
-    assert res == True
+    string_Utils = StringUtils()
+    res = string_Utils.is_empty("")
+    assert res is True
 
 def test_positive_is_emply2():
-    stringUtils = StringUtils()
-    res = stringUtils.is_empty("Saint Petersburg")
-    assert res == False
+    string_Utils = StringUtils()
+    res = string_Utils.is_empty("Saint Petersburg")
+    assert res is False
 
 # Преобразует список элементов в строку с указанным разделителем \n 
 def test_positive_list_to_str():
-    stringUtils = StringUtils()
-    res = stringUtils.list_to_string([1,2,3,4,5,6,7,8])
+    string_Utils = StringUtils()
+    res = string_Utils.list_to_string([1,2,3,4,5,6,7,8])
     assert res == "1, 2, 3, 4, 5, 6, 7, 8"
 
 def test_positive_list_to_str2():
-    stringUtils = StringUtils()
-    res = stringUtils.list_to_string(["Санкт", "Петербург"])
+    string_Utils = StringUtils()
+    res = string_Utils.list_to_string(["Санкт", "Петербург"])
     assert res == "Санкт, Петербург"
 
 def test_positive_list_to_str3():
-    stringUtils = StringUtils()
-    res = stringUtils.list_to_string(["Санкт", "Петербург"], "-")
+    string_Utils = StringUtils()
+    res = string_Utils.list_to_string(["Санкт", "Петербург"], "-")
     assert res == "Санкт-Петербург"
